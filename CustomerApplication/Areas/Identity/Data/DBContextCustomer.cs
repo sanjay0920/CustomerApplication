de04eca2cc46aacf3abc30d0b29b641ab2207fa7
sanjay bharathi <sanjaybharathis18087@it.ssn.edu.in>
2023-09-08 16:49:25 +0530

    Add project files.

---
+++ CustomerApplication/Areas/Identity/Data/DBContextCustomer.cs
@@ -0,0 +1,33 @@
+﻿using CustomerApplication.Areas.Identity.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CustomerApplication.Areas.Identity.Data;
+
+public class DBContextCustomer : IdentityDbContext<SampleUser>
+{
+    public DBContextCustomer(DbContextOptions<DBContextCustomer> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        // Customize the ASP.NET Identity model and override the defaults if needed.
+        // For example, you can rename the ASP.NET Identity table names and more.
+        // Add your customizations after calling base.OnModelCreating(builder);
+        builder.ApplyConfiguration(new ApplicationUserEntityConfiguration());
+    }
+}
+
+public class ApplicationUserEntityConfiguration : IEntityTypeConfiguration<SampleUser>
+{
+    public void Configure(EntityTypeBuilder<SampleUser> builder)
+    {
+        builder.Property(x => x.FirstName).HasMaxLength(50);
+        builder.Property(x => x.LastName).HasMaxLength(50);
+    }
+}
---
+++ CustomerApplication/Program.cs
@@ -0,0 +1,39 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using CustomerApplication.Areas.Identity.Data;
+var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("DBContextCustomerConnection") ?? throw new InvalidOperationException("Connection string 'DBContextCustomerConnection' not found.");
+
+builder.Services.AddDbContext<DBContextCustomer>(options =>
+    options.UseSqlServer(connectionString));
+
+builder.Services.AddDefaultIdentity<SampleUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<DBContextCustomer>();
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+app.UseAuthentication();;
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.MapRazorPages();
+app.Run();
